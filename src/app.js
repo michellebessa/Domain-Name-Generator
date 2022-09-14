b96ffa8c88af7de43c9b8da4508cb289e086335a
michellebessa <michelleandressa12@gmail.com>
2022-09-14 23:29:50 +0000

    whatever

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let domainNameGenerator = () => {
+  const pronoun = ["the", "our", "their", "her", "his", "my"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const adj = [
+    "great",
+    "big",
+    "cute",
+    "beautiful",
+    "messy",
+    "crazy",
+    "funny",
+    "weird",
+    "nice",
+    "cool"
+  ];
+  const noun = [
+    "cat",
+    "dog",
+    "bunny",
+    "goat",
+    "gnome",
+    "lizard",
+    "geico",
+    "bird"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const extension = [".com", ".org", ".net", ".us"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let m = 0; m < adj.length; m++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let k = 0; b < extension.length; k++) {}
+      }
+    }
+  }
 };
+
+document.querySelector("#the-generator").innerHTML = domainNameGenerator();
